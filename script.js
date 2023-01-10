176b2e01cfecf95df2efb1042deef2e128328e49
Victory Odumeh <vodumeh@gmail.com>
2023-01-10 14:21:09 +0100

    first save

---
+++ script.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('.menu');
+var links = document.querySelector('.links');
+var mark = document.querySelector('#cancel');
+
+function appear(){
+    links.style.right = "2px";
+}
+
+function disappear(){
+    links.style.right = "-300px";
+}
