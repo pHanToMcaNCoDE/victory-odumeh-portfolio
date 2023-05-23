05e8c306037d18afe0d45b25c0189d9c90e96b3c
Victory Odumeh <vodumeh@gmail.com>
2023-05-23 14:26:20 +0100

    responsive design

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ var menu = document.querySelector('.menu');
 var links = document.querySelector('.links');
 var mark = document.querySelector('#cancel');
 
-function appear(){
-    links.style.right = "2px";
-}
+// function appear(){
+//     links.style.right = "2px";
+// }
 
-function disappear(){
-    links.style.right = "-300px";
-}
+// function disappear(){
+//     links.style.right = "-300px";
+// }
+
+menu.addEventListener('click', () =>{
+    links.classList.add('active');
+    links.style.transition = "all .5s ease-in-out";
+});
+
+
+mark.addEventListener('click', () =>{
+    links.classList.remove('active');
+    links.style.transition = "all .5s ease-in-out";
+});
